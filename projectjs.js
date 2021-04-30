a626a2e894cf9e79fbdf04e207f80eb5540cff3d
fuyeukling <81092324+fuyeukling@users.noreply.github.com>
2021-04-30 22:41:30 +0800

    Add files via upload

---
+++ projectjs.js
@@ -0,0 +1,62 @@
+function myFunction1n() {
+    document.getElementById("nike1size").innerHTML ="<p style='color: #987777'>size avalible: <br>XS S <del>M</del> L</p>";
+}
+function myFunction2n() {
+    document.getElementById("nike2size").innerHTML = "<p style='color: #987777'>size avalible:<br> <del>XS</del> S M L XL</p>";
+}
+function myFunction3n() {
+    document.getElementById("nike3size").innerHTML = "<p style='color: #987777'>size avalible:<br> XS S M L XL</p>";
+}
+function myFunction4n() {
+    document.getElementById("nike4size").innerHTML = "<p style='color: #987777'>size avalible:<br> XS S M L XL</p>";
+}
+function myFunction5n() {
+    document.getElementById("nike5size").innerHTML = "<p style='color: #987777'>size avalible:<br><del>XS</del> S M L XL</p>";
+}
+function myFunction6n() {
+    document.getElementById("nike6size").innerHTML = "<p style='color: #987777'>size avalible:<br><del>XS</del> S M L XL</p>";
+}
+
+function myFunction1a() {
+    document.getElementById("adidas1size").innerHTML ="<p style='color: #987777'>size avalible: XS S <del>M</del> L XL</p>";
+}
+function myFunction2a() {
+    document.getElementById("adidas2size").innerHTML ="<p style='color: #987777'>size avalible: <del>XS</del> S M L XL</p>";
+}
+function myFunction3a() {
+    document.getElementById("adidas3size").innerHTML = "<p style='color: #987777'>size avalible:<br> XS S M L XL</p>";
+}
+function myFunction4a() {
+    document.getElementById("adidas4size").innerHTML = "<p style='color: #987777'>size avalible:<br> XS S M L XL</p>";
+}
+function myFunction5a() {
+    document.getElementById("adidas5size").innerHTML = "<p style='color: #987777'>size avalible:<br> <del>XS</del> S M L XL</p>";
+}
+function myFunction6a() {
+    document.getElementById("adidas6size").innerHTML = "<p style='color: #987777'>size avalible:<br> <del>XS</del> <del>S</del> M L XL</p>";
+}
+
+function myFunctionalert() {
+    alert("We are the clothing shop that gather different that best suit for you :)");
+}
+
+
+function bigImg(x) {
+    x.style.height = "44px";
+    x.style.width = "88px";
+}
+
+function normalImg(x) {
+    x.style.height = "34px";
+    x.style.width = "65px";
+}
+
+function bigImg2(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+}
+
+function normalImg2(x) {
+    x.style.height = "80px";
+    x.style.width = "80px";
+}
